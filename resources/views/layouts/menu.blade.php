7f9afd897f7915c7daa9ae09df487fe5ec818329
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-08-09 22:22:14 +0600

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,19 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <li class="nav-item">
     <a href="{{ route('admin.wareHouses.index') }}"
        class="nav-link {{ Request::is('admin/wareHouses*') ? 'active' : '' }}">
