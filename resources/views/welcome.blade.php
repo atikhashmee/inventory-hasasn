387f80d2028059fe403cbddf7c2e26692eb05e63
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-08-13 01:30:43 +0600

    issue fixing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,11 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        @if (auth()->user()->role=='admin')
+                            <a href="{{ route('admin.home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        @else
+                            <a href="{{ route('user.home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        @endif
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
