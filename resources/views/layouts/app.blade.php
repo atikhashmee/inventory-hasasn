be03e43694dc8dde6971119f0b44a69a561ce723
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-08-20 13:08:29 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,7 +149,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/1.3/bootstrapSwitch.min.js"
         integrity="sha512-DAc/LqVY2liDbikmJwUS1MSE3pIH0DFprKHZKPcJC7e3TtAOzT55gEMTleegwyuIWgCfOPOM8eLbbvFaG9F/cA=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="{{asset('assets/lib/datatable/js/jquery.dataTables.min.js')}}"></script>
+<script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script>
     $(function () {
         bsCustomFileInput.init();
