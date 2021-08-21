ee36e1147d20340e90914f36296feac5f775ebc1
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-08-21 17:04:58 +0600

    Update user.blade.php

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -13,7 +13,7 @@
   <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="{{route('user.home')}}">{{auth()->user()->shop->name}}</a>
+        <a class="navbar-brand" href="{{route('user.home')}}">{{(auth()->user()->shop)?auth()->user()->shop->name:'N/A'}}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
