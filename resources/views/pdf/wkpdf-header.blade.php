3edd6cf409a6ae7991d40ea0913f89fb2f1e3e1d
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-12-28 14:17:33 +0600

    Update wkpdf-header.blade.php

--- resources/views/pdf/wkpdf-header.blade.php
+++ resources/views/pdf/wkpdf-header.blade.php
@@ -18,94 +18,38 @@
         }
         .invoice-wrapper {
             position:relative;
-            width:100%;
+            width:890px;
             height:200px;
             top:80px;
             margin-bottom:10px;
-        }
-        .horizonatal-bar {
-            position: fixed;
-            left: 0;
-            top: 0;
-            right:0;
-            width:100%;
-        }
-        .horizonatal-bar img {
-            width: 120%;
-            position: fixed;
-            left: 0;
-            top: 63px;
-            z-index:-1;
-        }
-        .header-left-section{
-            margin-top: 20px;
-            width: 40%;
-            text-align: center;
-            position: relative;
-            height:150px;
-        }
-        .header-right-section{
-            text-align: center;
-            line-height: 20px;
-            padding-top: 10px;
-            height:150px;
-        }
-        
+        }        
         .top-header{
             font-size: 25px;
             text-transform: uppercase;
             color: red;
             margin-bottom: 10px;
         }
-        .website-text {
-            color: red;
-            font-size: 18px; 
-        }
-        
-        .qr-code {
-            width: 80px;
-            position: absolute;
-            bottom: 0;
-            left: 43%;
-        }
-        .logo {
-            position: absolute;
-            top: -10%;
-            left: 40%;
-            width: 120px;
-        }
-        .logo img, .qr-code img{
-            height:75px;
-        }
     </style>
 </head>
 <body>
-    
     <div class="invoice-wrapper">
-        <div class="horizonatal-bar">
-            <img src="{{ asset("assets/img/bar.png") }}" alt="" style="width:100%">
-        
-        </div>
+        <img src="{{ asset("assets/img/bar.png") }}" alt="" style="width: 100%;">
     
-    <table>
-        <tr>
-            <td class="header-left-section">
-                <div class="logo">
-                    <img src="{{$shop->image_link}}" alt="">
-                    <br>
+        <table style="width: 830px; position: relative; top: -120px;">
+            <tr>
+                <td style="text-align: center; width: 340px; vertical-align: top;">
+                    <img src="{{$shop->image_link}}" style="height: 90px;">
+                    <div style="width: 100%; margin-bottom: 8px;"></div>
                     <img src="https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=stuff" alt="">
-                </div>
-                
-            </td>
-            <td class="header-right-section">
-                
-                <div style="width: 100%; position:relative; left:50%">
-                    <h4 class="top-header">{{$shop->name}}</h4>
-                    {!!$shop->address!!}
-                </div>
-            </td>
-        </tr>
-    </table>
+                </td>
+                <td style="text-align: center; vertical-align: top;">
+                    <div style="width: 100%; position: relative; top: 40px;">
+                        <h4 class="top-header">{{$shop->name}}</h4>
+                        {!! $shop->address !!}
+                    </div>
+                </td>
+            </tr>
+        </table>
     </div>
     
     
